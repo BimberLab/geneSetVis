5971bca0fa99ff2278d1c00c0eac182a2b84a4c5
bbimber <bbimber@gmail.com>
2020-12-30 16:25:06 -0800

    Add test

--- tests/run_tests.R
+++ tests/run_tests.R
@@ -1,3 +1,1 @@
-testthat::test_that("Shiny app works", {
-	shinytest::expect_pass(shinytest::testApp('../inst/app', compareImages = TRUE))
-})
+shinytest::testApp('../inst/app', compareImages = TRUE)
